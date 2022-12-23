9c94ab8335cb97f113a2357db105ec2c331d1978
CPetchyotin <bosschonnanut@gmail.com>
2022-12-23 13:39:33 +0700

    Workshop7[23-12-2022]

---
+++ OOP_Program/src/CheckStockProduct.java
@@ -0,0 +1,52 @@
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class CheckStockProduct {
+	static double sum = 0;
+
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+		DecimalFormat f = new DecimalFormat("#,###.00");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("How many product list in stock : ");
+		int hmpl = sc.nextInt();
+		System.out.println();
+		Product[] productList = new Product[hmpl];
+		for (int i = 0; i < productList.length; i++) {
+			productList[i] = new Product();
+			System.out.print("Input product  Id    : ");
+			productList[i].setId(sc.next());
+			System.out.print("Input product  Unit  : ");
+			productList[i].setUnit(sc.nextInt());
+			while (productList[i].getUnit() <= 0) {
+				System.out.print("Input product Unit,again  : ");
+				productList[i].setUnit(sc.nextInt());
+			}		
+			System.out.println();
+		}
+		System.out.println("------------------------------------------------------------");
+		System.out.println("List of product in 'LOW' status.");
+		System.out.println("------------------------------------------------------------");
+		for (Product show : productList) {
+			if(show.getUnit()<5) {
+				System.out.println(">> "+show.getId()+" has "+ show.getUnit()+ " units");
+			}
+		}
+		System.out.println("------------------------------------------------------------");
+		System.out.println("List of product in 'NORMAL' status.");
+		System.out.println("------------------------------------------------------------");
+		for (Product show : productList) {
+			if(show.getUnit()>=5&&show.getUnit()<=50) {
+				System.out.println(">> "+show.getId()+" has "+ show.getUnit()+ " units");
+			}
+		}
+		System.out.println("------------------------------------------------------------");
+		System.out.println("List of product in 'HIGH' status.");
+		System.out.println("------------------------------------------------------------");
+		for (Product show : productList) {
+			if(show.getUnit()>50) {
+				System.out.println(">> "+show.getId()+" has "+ show.getUnit()+ " units");
+			}
+		}
+	}
+}
--- OOP_Program/src/StockProduct.java
+++ OOP_Program/src/StockProduct.java
@@ -2,36 +2,39 @@ import java.text.DecimalFormat;
 import java.util.*;
 
 public class StockProduct {
-static double sum = 0;
+	static double sum = 0;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+// TODO Auto-generated method stub
 		DecimalFormat f = new DecimalFormat("#,###.00");
 		Scanner sc = new Scanner(System.in);
 		Product[] productList = new Product[4];
 		for (int i = 0; i < productList.length; i++) {
 			productList[i] = new Product();
-			System.out.print("Input product   Id    : ");
+			System.out.print("Input product Id    : ");
 			productList[i].setId(sc.next());
-			System.out.print("Input product	Unit  : ");
+			System.out.print("Input product Unit  : ");
 			productList[i].setUnit(sc.nextInt());
-			while(productList[i].getUnit()<=0) {
-				System.out.print("Input product	Unit,again  : ");
+			while (productList[i].getUnit() <= 0) {
+				System.out.print("Input product Unit,again    : ");
 				productList[i].setUnit(sc.nextInt());
 			}
-			System.out.print("Input product   Price : ");
+			System.out.print("Input product Price : ");
 			productList[i].setPrice(sc.nextDouble());
-			while(productList[i].getPrice()<=0) {
-				System.out.print("Input product	Unit,again  : ");
+			while (productList[i].getPrice() <= 0) {
+				System.out.print("Input product Unit,again : ");
 				productList[i].setPrice(sc.nextDouble());
 			}
 			System.out.println();
-			for(Product show : productList) {
-				System.out.println("Product ID : "+show.getId()+", Total Price = "+f.format(show.calculate())+"baht.");
-				sum+=show.calculate();
-			}
 		}
-		System.out.print("------------------------------------------------------------");
-		System.out.println("Total price of all product is "+f.format(sum)+" baht.");
+		System.out.println("------------------------------------------------------------");
+		for (Product show : productList) {
+			System.out.println(
+					"Product ID : " + show.getId() + ", Total Price = " + f.format(show.calculate()) + "baht.");
+			sum += show.calculate();
+		}
+		System.out.println("------------------------------------------------------------");
+		System.out.println("Total price of all product is " + f.format(sum) + " baht.");
 
 	}
 }
